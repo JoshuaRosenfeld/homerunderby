2715da00bef60c9ffedc3f4b5279107d011b9636
Joshua Rosenfeld <jlr0802@gmail.com>
2014-07-12 19:23:47 -0400

    update section ranks and values

--- js/sections.js
+++ js/sections.js
@@ -1,115 +1,115 @@
 var sections = [
 	{
 		"name": 128,
-		"homeruns": 5.9,
+		"homeruns": 8.34,
 		"avg_price": 834.87,
-		"rank": 6
+		"rank": 7
 	},
 	{
 		"name": 129,
-		"homeruns": 3.2,
+		"homeruns": 4.17,
 		"avg_price": 727.63,
-		"rank": 14
+		"rank": 12
 	},
 	{
 		"name": 130,
-		"homeruns": 2.7,
+		"homeruns": 3.48,
 		"avg_price": 752.36,
 		"rank": 15
 	},
 	{
 		"name": 131,
-		"homeruns": 2.7,
+		"homeruns": 3.48,
 		"avg_price": 701.67,
-		"rank": 13
+		"rank": 14
 	},
 	{
 		"name": 132,
-		"homeruns": 0.5,
+		"homeruns": 0.7,
 		"avg_price": 732.06,
-		"rank": 26
+		"rank": 25
 	},
 	{
 		"name": 133,
-		"homeruns": 1.1,
+		"homeruns": 1.39,
 		"avg_price": 522.83,
-		"rank": 24
+		"rank": 21
 	},
 	{
 		"name": 134,
 		"homeruns": 0,
 		"avg_price": 473.5,
-		"rank": 30
+		"rank": 29
 	},
 	{
 		"name": 135,
 		"homeruns": 0,
 		"avg_price": 453,
-		"rank": 29
+		"rank": 28
 	},
 	{
 		"name": 136,
-		"homeruns": 3.2,
+		"homeruns": 4.87,
 		"avg_price": 483.20,
-		"rank": 8
+		"rank": 6
 	},
 	{
 		"name": 137,
-		"homeruns": 0.5,
+		"homeruns": 0.7,
 		"avg_price": 493.00,
 		"rank": 23
 	},
 	{
 		"name": 138,
-		"homeruns": 2.2,
+		"homeruns": 2.78,
 		"avg_price": 706,
-		"rank": 19
+		"rank": 18
 	},
 	{
 		"name": "great-clips-great-seats",
-		"homeruns": 2.7,
+		"homeruns": 4.17,
 		"avg_price": 618.75,
 		"rank": 10
 	},
 	{
 		"name": 229,
-		"homeruns": 6.5,
+		"homeruns": 8.34,
 		"avg_price": 772,
 		"rank": 5
 	},
 	{
 		"name": 230,
-		"homeruns": 4.8,
+		"homeruns": 6.95,
 		"avg_price": 532.00,
 		"rank": 4
 	},
 	{
 		"name": 231,
-		"homeruns": 3.2,
+		"homeruns": 4.17,
 		"avg_price": 542.34,
 		"rank": 9
 	},
 	{
 		"name": 232,
-		"homeruns": 2.7,
+		"homeruns": 3.48,
 		"avg_price": 525,
-		"rank": 1
+		"rank": 11
 	},
 	{
 		"name": 233,
-		"homeruns": 0.5,
+		"homeruns": 0.7,
 		"avg_price": 550,
-		"rank": 25
+		"rank": 24
 	},
 	{
 		"name": 234,
-		"homeruns": 1.1,
+		"homeruns": 1.39,
 		"avg_price": 319.6,
 		"rank": 16
 	},
 	{
 		"name": 235,
-		"homeruns": 0.5,
+		"homeruns": 0.7,
 		"avg_price": 470.83,
 		"rank": 22
 	},
@@ -117,67 +117,67 @@ var sections = [
 		"name": 236,
 		"homeruns": 0,
 		"avg_price": 504.44,
-		"rank": 18
+		"rank": 30
 	},
 	{
 		"name": 237,
-		"homeruns": 2.7,
+		"homeruns": 3.48,
 		"avg_price": 830.67,
 		"rank": 17
 	},
 	{
 		"name": 238,
-		"homeruns": 5.4,
+		"homeruns": 6.95,
 		"avg_price": 407.84,
 		"rank": 1
 	},
 	{
 		"name": 239,
-		"homeruns": 4.8,
+		"homeruns": 6.26,
 		"avg_price": 383.59,
 		"rank": 2
 	},
 	{
 		"name": 240,
-		"homeruns": 3.8,
+		"homeruns": 4.87,
 		"avg_price": 1253.25,
-		"rank": 20
+		"rank": 19
 	},
 	{
 		"name": 329,
-		"homeruns": 2.2,
+		"homeruns": 2.78,
 		"avg_price": 305.78,
-		"rank": 7
+		"rank": 8
 	},
 	{
 		"name": 330,
-		"homeruns": 1.1,
+		"homeruns": 1.39,
 		"avg_price": 269,
-		"rank": 12
+		"rank": 13
 	},
 
 	{
 		"name": 331,
-		"homeruns": 2.7,
+		"homeruns": 3.48,
 		"avg_price": 230.75,
 		"rank": 3
 	},
 	{
 		"name": 332,
-		"homeruns": 1.1,
+		"homeruns": 1.39,
 		"avg_price": 424.63,
-		"rank": 21
+		"rank": 20
 	},
 	{
 		"name": 333,
 		"homeruns": 0,
 		"avg_price": 283.4,
-		"rank": 28
+		"rank": 27
 	},
 	{
 		"name": 334,
 		"homeruns": 0,
 		"avg_price": 279.27,
-		"rank": 27
+		"rank": 26
 	}
 ];
